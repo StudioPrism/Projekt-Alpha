5fbf8a84f9910650eac61f103e300432e14b0c20
Jordan Neal Pryor <jnpryor@student.fullsail.edu>
2026-05-02 08:04:11 -0500

    Building a Dedicated FPS Controller
    Still Exploring Options on this Project, so Expect multiple Components to Be implemented, changed And/or Removed

---
+++ Code/Player/cameraController.cs
@@ -0,0 +1,37 @@
+// Copyright Studio Prism. Licensed under PolyForm Shield 1.0.0.
+// https://polyformproject.org/licenses/shield/1.0.0
+// Required Notice: Copyright Studio Prism (https://github.com/studioprism)
+//
+// cameraController.cs 
+//
+// 
+using Sandbox;
+
+public sealed class cameraController : Component
+{
+	[Property] public GameObject cameraObject{ get; set;}
+	[Property, Range(40f, 80f)] public float eyeHeight{ get; set;} 
+	[Property, Range(60f, 89f)] public float pitchClamp{ get; set;}
+	[Sync] public Angles eyeAngles{ get; set;}
+	public Vector3 aimDirection => eyeAngles.ToRotation().Forward;
+
+	protected override void OnUpdate()
+	{
+		if ( IsProxy ) return;
+		// Track/Accumulate Look input
+		var look = eyeAngles;
+		look += Input.AnalogLook;
+		// Clamp Pitch
+		look.pitch = look.pitch.Clamp(-pitchClamp, pitchClamp);
+		look.roll = 0f; // Kill any Roll that could accumulate.
+		eyeAngles = look;
+		// Apply to Camera, No Smoothing or Lerp
+		if ( cameraObject.IsValid() )
+		{
+			cameraObject.WorldRotation = eyeAngles.ToRotation();
+			cameraObject.WorldPosition = WorldPosition + Vector3.Up * eyeHeight;
+		}
+	}
+	public Rotation BodyRotation => Rotation.FromYaw( eyeAngles.yaw );
+}
+
